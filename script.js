06396e878e79014b43a532a4785a9173a3b81cfb
Azkieel <red.ezequiel@gmail.com>
2022-04-21 22:47:57 +0800

    write css

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ const operatorDisplay = document.querySelector('.operators');
 
 const arrayOperator = ['/', '*', '-', '+']
 
-function numButtonMaker() {
+function buttonGenerator() {
     for (i = 0; i < 10 ; i++) {
         const numButtons = document.createElement('button');
         numButtons.textContent = i;
+        numButtons.className = 'num-btn'
         numDisplay.append(numButtons);
     }
-    
-
+    for (i = 0; i < arrayOperator.length; i++) {
+        const operatorBtn = document.createElement('button');
+        operatorBtn.textContent = arrayOperator[i];
+        operatorBtn.className = 'opr-btn';
+        operatorDisplay.append(operatorBtn);
+    }
 }
 
-numButtonMaker();
-
+buttonGenerator();
 
 
