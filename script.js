3a913f8ba2fc8b80bcf9a7970db0e2ad95b8b789
Azkieel <red.ezequiel@gmail.com>
2022-04-28 01:09:35 +0800

    write data-key for keyboard support

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const numDisplay = document.querySelector('.numbers');
 const operatorDisplay = document.querySelector('.operators');
 const funcDisplay = document.querySelector('.functions');
 
-const arrayOperator = ['*', '-', '+', '=']
+const arrayOperator = ['x', '-', '+', '=']
 const arrayFunc = ['C', 'Del', '-/+', '/']
 
 function buttonGenerator() {
-    for (i = 1; i < 10 ; i++) {
+    for (i = 1, j = 97; i < 10, j < 106; i++, j++) {
         const numButtons = document.createElement('button');
         numButtons.textContent = i;
-        numButtons.className = 'num-btn'
+        numButtons.className = 'num-btn';
+        numButtons.setAttribute('data-key', [j]);
         numDisplay.append(numButtons);
     }
     for (i = 0; i < arrayOperator.length; i++) {
@@ -19,6 +20,13 @@ function buttonGenerator() {
         if (arrayOperator[i] === arrayOperator [3]) {
             operatorBtn.classList.remove('operator-func');
             operatorBtn.classList.add('equal-btn');
+            operatorBtn.setAttribute('data-key', '13');
+        } else if (arrayOperator[i] === arrayOperator [0]) {
+            operatorBtn.setAttribute('data-key', '106');
+        }  else if (arrayOperator[i] === arrayOperator [1]) {
+            operatorBtn.setAttribute('data-key', '109');
+        }  else if (arrayOperator[i] === arrayOperator [2]) {
+            operatorBtn.setAttribute('data-key', '107');
         }
         operatorDisplay.append(operatorBtn);
     }
@@ -28,12 +36,16 @@ function buttonGenerator() {
         funcBtn.className = 'func-btn';
         if (arrayFunc[i] === arrayFunc[3]) {
             funcBtn.classList.replace('func-btn', 'opr-btn');
+            funcBtn.classList.add('operator-func');
         } else if (arrayFunc[i] === arrayFunc[0]) {
             funcBtn.classList.add('cancel-btn');
         } else if (arrayFunc[i] === arrayFunc[1]) {
             funcBtn.classList.add('del-btn');
+            funcBtn.setAttribute('data-key', '8');
         } else if (arrayFunc[i] === arrayFunc[2]) {
             funcBtn.classList.add('sign-btn');
+        } else if (arrayFunc[i] === arrayFunc[3]) {
+            funcBtn.setAttribute('data-key', '111');
         }
         funcDisplay.append(funcBtn);
     }
@@ -42,16 +54,28 @@ buttonGenerator();
 
 const calc = {
     add: function (a, b) {
-        return a + b;
+        if (Number.isInteger(a + b)) {
+            return a + b;
+        }
+        return (Math.round((a+b) * 100) / 100).toFixed(2);
     },
     subtract: function (a, b) {
-        return a - b;
+        if (Number.isInteger(a - b)) {
+            return a - b;
+        }
+        return (Math.round((a-b) * 100) / 100).toFixed(2);
     },
     multiply: function (a, b) {
-        return a*b;
+        if (Number.isInteger(a * b)) {
+            return a * b;
+        }
+        return (Math.round((a*b) * 100) / 100).toFixed(2);
     },
     divide: function (a, b) {
-        return a/b;
+        if (Number.isInteger(a / b)) {
+            return a / b;
+        }
+        return (Math.round((a/b) * 100) / 100).toFixed(2);
     },
 }
 
@@ -173,7 +197,6 @@ function integerFix() {
     (Math.round(b * 100) / 100).toFixed(2);
     num1 = parseFloat(a);
     num2 = parseFloat(b);
-    (Math.round(result * 100) / 100).toFixed(2);
 }
 
 function operateMethod(a, b, opr) {
@@ -186,7 +209,7 @@ function operateMethod(a, b, opr) {
     } else if (opr === '-') {
         result = calc.subtract(a,b);
         display2.textContent = result;
-    } else if (opr === '*') {
+    } else if (opr === 'x') {
         result = calc.multiply(a,b)
         display2.textContent = result;
     } else if (opr === '/') {
